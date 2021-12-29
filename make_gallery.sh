998661a030b6f9965f9fc32d8682066e2c1bb3c5
ropapermaker <rojdi.thomallari@gmail.com>
2021-12-29 23:29:01 +0100

    first commit with all pictures

---
+++ make_gallery.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+# set -e
+
+# Usage: ./make_gallery.sh
+#
+# Run in a directory with a "papes/" subdirectory, and it will create a
+# "thumbnails/" subdirectory.
+#
+# Uses imagemagick's `convert`, so make sure that's installed.
+# On Nix, nix-shell -p imagemagick --run ./make_gallery.sh
+
+rm -rf thumbnails/*
+mkdir -p thumbnails
+
+url_root="https://raw.githubusercontent.com/ropapermaker/cutecato/master"
+
+echo "## cutecato Collection" >README.md
+echo "" >>README.md
+
+total=$(ls papes/ | wc -l)
+i=0
+
+for src in papes/*; do
+  ((i++))
+  filename="$(basename "$src")"
+  printf '%4d/%d: %s\n' "$i" "$total" "$filename"
+
+  convert -resize 200x "$src" "${src/papes/thumbnails}"
+
+  filename_escaped="${filename// /%20}"
+  thumb_url="$url_root/thumbnails/$filename_escaped"
+  pape_url="$url_root/papes/$filename_escaped"
+
+  echo "[![$filename]($thumb_url)]($pape_url)" >>README.md
+done
